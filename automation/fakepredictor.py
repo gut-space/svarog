c74f3a47838e8d90069be1dc4055ac7c18bf324e
Slawomir Figiel <fivitti@gmail.com>
2019-12-25 22:34:23 +0100

    Fix inccorect commands. Now records the signal.

--- automation/fakepredictor.py
+++ automation/fakepredictor.py
@@ -3,9 +3,10 @@ from orbit_predictor.predictors.base import PredictedPass
 import datetime as dt
 
 class FakePredictor():
-    def __init__(self, sate_id: str, delay: int):
+    def __init__(self, sate_id: str, delay: int, after: int):
         self._sate_id = sate_id
         self._delay = delay
+        self._after = after
 
     def get_next_pass(self, location, when_utc=None, max_elevation_gt=5,
                       aos_at_dg=0, limit_date=None):
@@ -13,6 +14,7 @@ class FakePredictor():
             when_utc = dt.datetime.utcnow()
         
         delay = self._delay
+        after = self._after
         return PredictedPass(location, self._sate_id, random.randint(aos_at_dg, 90),
-            when_utc + dt.timedelta(seconds=10), when_utc + dt.timedelta(seconds=10 + delay), delay,
-            None, when_utc + dt.timedelta(seconds=10 + delay / 2))
+            when_utc + dt.timedelta(seconds=after), when_utc + dt.timedelta(seconds=after + delay), delay,
+            None, when_utc + dt.timedelta(seconds=after + delay / 2))
--- automation/main.py
+++ automation/main.py
@@ -8,10 +8,10 @@ from orbit_predictor.locations import Location
 
 import fakepredictor
 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s: %(message)s')
 scheduler = sched.scheduler()
-logger = logging.getLogger(__name__)
 
-HAS_SDR = False
+HAS_SDR = True
 
 # Prediction constants
 NOAA_URL = r"https://celestrak.com/NORAD/elements/noaa.txt"
@@ -19,7 +19,7 @@ location = Location("Taneczna", 18, 52, 153)
 SATELLITE = "NOAA 18"
 AOS_AT_DG = 30
 FREQUENCY = "137.9125e6"
-BANDWIDTH = "2048000"
+SAMPLE_RATE = "11025"
 
 def get_now() -> datetime.datetime:
     return datetime.datetime.utcnow()
@@ -30,54 +30,57 @@ def get_predictor():
         predictor = source.get_predictor(SATELLITE)
         return predictor
     else:
-        return fakepredictor.FakePredictor(SATELLITE, 60)
+        return fakepredictor.FakePredictor(SATELLITE, 15, 2)
 
-def record(frequency: str, bandwidth: str, output_filename: str):
+def record(frequency: str, sample_rate: str, output_filename: str):
     '''
-    rtl_sdr -f 137.620e6 -s 2048000 -g 29 -p 22 | sox -t raw -b 16 -e signed-integer -r 11025 -B -c1 NOAA15.wav
+    rtl_fm -f 137.620e6 -s 2048000 -g 29 -p 22 | sox -t raw -b 16 -e signed-integer -r 11025 -B -c1 NOAA15.wav
     '''
 
     if not HAS_SDR:
-        print("Recording...", end="")
-        return lambda: print("Stop recording!")
+        return lambda: None
 
     rtl_process = subprocess.Popen(
-        ["rtl_sdr", 
+        ["rtl_fm", 
          "-f", frequency,
-         "-s", bandwidth,
-         "-g", 50,
-         "-p", 22
+         "-s", sample_rate,
+         "-g", "29",
+         "-p", "22",
+         "-"
         ], stdout=subprocess.PIPE
     )
     sox_process = subprocess.Popen(
         ["sox",
          "-t", "raw",
-         "-b", 16,
-         "-e", "signed-integer",
-         "-r", 11025,
-         "-B",
-         "-c1",
+         "-b", "16",
+         "-e", "signed",
+         "-r", sample_rate,
+         "-c", "1",
+         "-", 
          output_filename], stdin=rtl_process.stdout
     )
 
+    print(rtl_process.args)
+    print(sox_process.args)
     terminate = lambda: rtl_process.terminate()
     return terminate
 
 def on_stop_recording(terminate):
     terminate()
-    logger.log(logging.DEBUG, "Stop recording")
+    logging.debug("Stop recording")
 
-def on_start_recording(los, frequency, bandwidth, output_filename):
+def on_start_recording(los, frequency, sample_rate, output_filename):
     now = get_now()
     delay = los - now
-    terminate = record(frequency, bandwidth, output_filename)
-    logger.log(logging.DEBUG, "Recording on %s with bandwith %s to file %s" % (frequency, bandwidth, output_filename))
+    terminate = record(frequency, sample_rate, output_filename)
+    logging.debug("Recording on %s with bandwith %s to file %s" % (frequency, bandwidth, output_filename))
     scheduler.enter(delay.total_seconds(), 1, on_stop_recording, (terminate,))
 
-def schedule_recording(frequency: str, bandwidth: str, output_filename: str, aos: datetime.datetime, los: datetime.datetime):    
+def schedule_recording(frequency: str, sample_rate: str, output_filename: str, aos: datetime.datetime, los: datetime.datetime):    
     now = get_now()
     delay = aos - now
-    scheduler.enter(delay.total_seconds(), 1, on_start_recording, (los, frequency, bandwidth, output_filename))
+    logging.debug("Schedule recording from %s to %s" % (str(aos), str(los)))
+    scheduler.enter(delay.total_seconds(), 1, on_start_recording, (los, frequency, sample_rate, output_filename))
 
 predictor = get_predictor()
 next_pass = predictor.get_next_pass(location, aos_at_dg=AOS_AT_DG)
@@ -85,7 +88,7 @@ next_pass = predictor.get_next_pass(location, aos_at_dg=AOS_AT_DG)
 timestamp = next_pass.aos.strftime("%Y-%m-%d %H:%M:%S")
 output_filename = "%s_%s_%s_%s.wav" % (SATELLITE, FREQUENCY, location.name, timestamp)
 
-schedule_recording(FREQUENCY, BANDWIDTH, output_filename, next_pass.aos, next_pass.los)
+schedule_recording(FREQUENCY, SAMPLE_RATE, output_filename, next_pass.aos, next_pass.los)
 
 scheduler.run()
 
