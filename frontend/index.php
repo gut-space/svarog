e663860a798888ebbc63ae217af929d83503dcb1
Tomek Mrugalski <tomasz@isc.org>
2020-01-03 00:41:25 +0100

    Observations list now has clickable satellite links

--- frontend/index.php
+++ frontend/index.php
@@ -37,20 +37,36 @@ function showStats($dbconn) {
     echo "<p> $obsCnt observations from $satsCnt satellite(s), between $startDate and $endDate.</p>";
 }
 
-function showSats($dbconn) {
+
+
+function getSats($dbconn) {
     // Performing SQL query
     $query = 'SELECT * FROM satellites';
     $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
+    $sats = array();
+
+    while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+        $sats[strtoupper($line["sat_name"])] = [ $line["sat_id"], $line["url"]];
+    }
+
+    // Free resultset
+    pg_free_result($result);
+
+    return $sats;
+}
+
+function showSats($sats) {
     // Printing results in HTML
     $cnt = 0;
     echo "<h3>Satellites</h3>";
     echo "<table class='table table-striped'>\n";
     echo "<tr><th>ID</th><th>Name</th></tr>";
-    while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+
+    foreach ($sats as $key => $val) {
         echo "\t<tr>\n";
-        echo "<td>" . $line["sat_id"] . "</td>";
-        echo "<td><a href=\"". $line["url"]. "\">". $line["sat_name"] . "</a></td>";
+        echo "<td>" . $key . "</td>";
+        echo "<td><a href=\"". $val[1]. "\">". $val[0] . "</a></td>";
 
         //var_dump($line);
         //foreach ($line as $col_value) {
@@ -61,8 +77,6 @@ function showSats($dbconn) {
     }
     echo "</table>\n";
 
-    // Free resultset
-    pg_free_result($result);
 }
 
 function showObservations($dbconn) {
@@ -99,7 +113,9 @@ function showObservations($dbconn) {
 $dbconn = pg_connect("host=localhost dbname=satnogs user=satnogs password=lie8Avie")
     or die('Could not connect: ' . pg_last_error());
 
-showStats($dbconn);
+$sats = getSats($dbconn);
+
+showStats($sats);
 
 showObservations($dbconn);
 
