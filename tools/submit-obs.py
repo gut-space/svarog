7d34e2e6589959811f869e540b774e5111f65869
Tomek Mrugalski <tomasz@isc.org>
2020-01-02 23:46:08 +0100

    updated submit-obs.py

--- tools/submit-obs.py
+++ tools/submit-obs.py
@@ -11,8 +11,19 @@
 # sat_name - name of the satellite
 # notes - a string with notes
 
+# SSH connection
+username="satnogs"
+
 hostname="satnogs.klub.com.pl"
-subdir="public_html/data"
+destdir="public_html/data"
+
+# Uncomment this for local deployment
+#hostname="localhost"
+#destdir="/var/www/html"
+
+# Postgres connection
+dbuser="satnogs"
+dbname="satnogs"
 
 import sys
 import os
@@ -20,16 +31,53 @@ import subprocess
 
 def submit_observation(path, sat_name, aos, tca, los, notes):
 
-    dst = "%s:%s" % (hostname, subdir)
-    print("Uploading file: cmd=[scp %s %s]" % (path, dst))
-    subprocess.run(["scp", path, dst])
-
     fnames = path.split("/")
     filename = fnames[-1]
 
-    cmd = "psql -c \"INSERT INTO observations(aos,sat_name,filename) VALUES('%s', '%s', '%s')\"" % (aos, sat_name, filename)
-    print("Adding record in the db: cmd=[%s]" % cmd)
-    subprocess.run(["ssh", hostname, cmd])
+    # First we need to make sure there's the destination directory
+    params = []
+    if hostname != "localhost":
+        params.append("ssh")
+        params.append(hostname)
+
+    params.append("mkdir")
+    params.append("-p")
+    params.append(("%s/%s" % (destdir, "data")))
+    subprocess.run(params)
+
+    # Second step is to copy (cp or scp) the file to its destination
+    params = []
+    if hostname != "localhost":
+        dst = "%s:%s/data" % (hostname, destdir)
+        params.append("scp")
+        print("Uploading file: cmd=[scp %s %s]" % (path, dst))
+    else:
+        dst = "%s/data" % destdir
+        params.append("cp")
+        print("Copying file locally: cp %s -> %s" % (path, dst))
+    params.append(path)
+    params.append(dst)
+
+    # Commented out
+    #subprocess.run(params)
+
+    # Finally, we need to create the DB entry
+    sqlcmd = "INSERT INTO observations(aos,sat_name,filename) VALUES('%s', '%s', '%s');" % (aos, sat_name, filename)
+    print("Adding record in the db: sqlcmd=[%s]" % sqlcmd)
+
+    params = []
+    if hostname != "localhost":
+        params.append("ssh")
+        params.append(hostname)
+        params.append("psql -c \"%s\" %s" % (sqlcmd, dbname))
+    else:
+        params.append("psql")
+        params.append("-U")
+        params.append(dbuser)
+        params.append("-c")
+        params.append(sqlcmd)
+        params.append(dbname)
+    subprocess.run(params)
 
 
 print("Detected python: %d.%d.%d" % (sys.version_info[0], sys.version_info[1], sys.version_info[2]))
