9b380891fbe224e7b3980ca3d6dcaafd506effaf
tomasz.mrugalski@gmail.com <satnogs@vps608537.ovh.net>
2020-02-13 21:29:05 +0100

    Updated thumbs.py to store thumbnails in a thumbs/ subdir

--- tools/thumbs.py
+++ tools/thumbs.py
@@ -4,15 +4,15 @@ from subprocess import run
 from os import listdir
 from os.path import isfile, join
 
-imgpath="/home/satnogs/public_html/data"
-
+imgpath="/home/satnogs/data"
+thumbpath="/home/satnogs/data/thumbs"
 
 pngfiles = [f for f in listdir(imgpath) if isfile(join(imgpath, f)) and f.endswith(".png")]
 
 for f in pngfiles:
     if f.startswith("thumb-"):
         continue
-    if isfile(join(imgpath, "thumb-" + f)):
+    if isfile(join(thumbpath, "thumb-" + f)):
         continue
     print("Generating thumbnail for %s" % f)
-    run(["convert", "-thumbnail", "200", join(imgpath, f), join(imgpath, "thumb-" + f)])
+    run(["convert", "-thumbnail", "200", join(imgpath, f), join(thumbpath, "thumb-" + f)])
