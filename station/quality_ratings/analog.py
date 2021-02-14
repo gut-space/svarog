708620823d3b03d4aaeca9a4f4eb52da29ee78ef
Slawomir Figiel <fivitti@gmail.com>
2021-02-14 17:39:51 +0100

    Docstrings for rate functions

--- station/quality_ratings/analog.py
+++ station/quality_ratings/analog.py
@@ -2,4 +2,7 @@ from analog_noise_estimator import estimate as estimate_analog_noise
 
 
 def rate(img):
+    '''Uses noise estimation based on Gauss distribution. Only for analog
+       noise in form similar to "groats" in old TV. Supports only 1D (grayscale)
+       images.'''
     return 1.0 - estimate_analog_noise(img)
