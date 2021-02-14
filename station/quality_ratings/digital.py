f01f237ada95a8c4058952b471d375d2bce19b29
Slawomir Figiel <fivitti@gmail.com>
2021-02-14 17:39:51 +0100

    Docstrings for rate functions

--- station/quality_ratings/digital.py
+++ station/quality_ratings/digital.py
@@ -1,6 +1,7 @@
 
 
 def rate(img):
+    """Calculate ratio between completly black pixels to all pixels"""
     noise_mask = img == 0
     if img.ndim == 3:
         noise_factors = noise_mask.sum(axis=2) == 3
