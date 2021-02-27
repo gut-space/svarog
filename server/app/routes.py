40b6e2586736eac247c875300c14bf6b9a62d7e0
Tomek Mrugalski <tomasz@isc.org>
2021-02-27 20:59:34 +0100

    Initial skeleton of a CZML exporter

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,7 +1,8 @@
-from flask import render_template, send_from_directory, send_file
+from flask import render_template, send_from_directory, send_file, Response
 from app import app
 # Load routes from modules in "controllers" directory
 from app.controllers import *
+from app.czml import get_czml
 
 @app.route('/')
 @app.route('/index')
@@ -14,4 +15,11 @@ def favicon():
 
 @app.route('/data/<path:path>')
 def send_js(path):
-    return send_from_directory('data', path)
+    return send_from_directory('/home/thomson/devel/aquarius/server/data', path)
+
+
+@app.route('/czml/<id>')
+def send_viewer(id):
+
+    return Response(get_czml(id), mimetype="application/json")
+
