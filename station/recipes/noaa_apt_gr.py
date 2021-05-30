7ea3283c8af94f5debbb5bc04d18d73634454e60
Tomek Mrugalski <tomek@isc.org>
2021-05-30 19:19:20 +0200

    Waterfall integrated

--- station/recipes/noaa_apt_gr.py
+++ station/recipes/noaa_apt_gr.py
@@ -3,7 +3,9 @@
 from contextlib import suppress
 from datetime import timedelta
 import os.path
+from os import remove
 import signal
+import waterfall
 
 import sh
 
@@ -16,11 +18,16 @@ def execute(working_dir: str, frequency: str, duration: timedelta, sh=sh):
     signal_path = os.path.join(working_dir, "signal.wav")
     product_path = os.path.join(working_dir, "product.png")
     log_path = os.path.join(working_dir, "session.log")
-    waterfall_path = os.path.join(working_dir, "waterfall.dat")
+    waterfall_path = os.path.join(working_dir, "waterfall.png")
+
+    # TODO: If enabled, bias-T should be enabled
+
+    # Here's the command that can be used to receive the transmission.
 
     #./satnogs_noaa_apt_decoder.py --soapy-rx-device="driver=airspy" --rx-freq 137.9125M --decoded-data-file-path=/home/pi/observations/satnogs/decoded.png
     #  --samp-rate-rx=3000000 --antenna RX --gain 45 --bw 41000
 
+    # TODO: Some of those parameters should be configurable.
     soapy_rx_device = "driver=airspy"
     sample_rate_rx = 3000000 # number of samples per second
     gain = 45 # 45 is max for airspy, rtlsdr can go up to 49.6
@@ -37,6 +44,9 @@ def execute(working_dir: str, frequency: str, duration: timedelta, sh=sh):
     logfile.write("bandwidth=%d" % bandwidth)
     logfile.flush()
 
+    waterfall_dat = waterfall_path + ".dat"
+
+
     # Run rtl_fm/rx_fm - this records the actual samples from the RTL device
     with suppress(sh.TimeoutException):
         sh.satnogs_noaa_apt_decoder(
@@ -52,7 +62,7 @@ def execute(working_dir: str, frequency: str, duration: timedelta, sh=sh):
             "--bw", bandwidth,
             # now set up the files
             "--decoded-data-file-path", product_path,
-            "--waterfall-file-path", waterfall_path,
+            "--waterfall-file-path", waterfall_dat,
             "--file-path", signal_path,
             _timeout=duration.total_seconds(),
             _timeout_signal=signal.SIGTERM,
@@ -61,6 +71,16 @@ def execute(working_dir: str, frequency: str, duration: timedelta, sh=sh):
             _err=logfile
         )
     logfile.flush()
+
+    # The GNU Radio pipeline generated raw waterfall data, which is reasonably big (30M or so).
+    # We want to convert it into easily browseable PNG file.
+    logfile.write("Converting waterfall data: %s to %s" % (waterfall_dat, waterfall_path))
+    w = waterfall.Waterfall(waterfall_dat)
+    w.plot(waterfall_path)
+
+    # Remove raw waterfall data, we don't need it.
+    os.remove(waterfall_dat)
+
     logfile.close()
 
     return [
