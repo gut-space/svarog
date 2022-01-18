c7975d0362ac916fa285470329fac75fae109eab
Tomek Mrugalski <tomek@isc.org>
2022-01-18 20:37:41 +0100

    Test fix

--- station/tests/test_receiver.py
+++ station/tests/test_receiver.py
@@ -8,7 +8,7 @@ class TestReceiverRatings(unittest.TestCase):
     def setUp(self):
         data_directory = os.path.join(os.path.dirname(__file__), "data")
         self.rgb_path = os.path.join(data_directory, "rgb-image.png")
-        self.gray_path = os.path.join(data_directory, "gray-image.png") 
+        self.gray_path = os.path.join(data_directory, "gray-image.png")
 
     def test_rating_for_single_layer_product(self):
         rating = receiver.get_rating_for_product(self.gray_path, "analog")
@@ -38,7 +38,8 @@ class TestReceiverRatings(unittest.TestCase):
 
     def test_single_layer_rating_called_with_multi_layer_product(self):
         rating = receiver.get_rating_for_product(self.rgb_path, "analog")
-        self.assertIsNone(rating)
+        # with the recent updates, the rating now works for RGB images
+        self.assertIsNotNone(rating)
 
     def test_multi_layer_rating_called_with_single_layer_product(self):
         rating = receiver.get_rating_for_product(self.gray_path, "digital")
